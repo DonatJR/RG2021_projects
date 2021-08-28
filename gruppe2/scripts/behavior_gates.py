- fix path follow behaviour with correct angle calculation - fix minimax behaviour with correct angle calculation - improve collision avoidance (division by zero) - do not use freespace with minimax
Co-authored-by: Jessica Kaechele <22886239+JessicaKaechele@users.noreply.github.com>

gruppe2/scripts/behavior_gates.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def OR(x,y):
-    return  x+y-x*y*np.tanh(x+y)/np.tanh(2)
+    return x+y-x*y*np.tanh(x+y)/np.tanh(2)
 
 def AND(x,y):
     return x*y*np.tanh(x+y)/np.tanh(2)

gruppe2/scripts/collision_avoidance_behaviour.py
@@ -1,3 +1,4 @@
+from numpy.core.numeric import NaN
 from helper_types import PosAndOrientation, AnimalProperties
 
 import numpy as np
@@ -5,31 +6,32 @@ import numpy as np
 class CollisionAvoidanceBehaviour():
     def __init__(self, animal_properties: AnimalProperties):
         self.animal_properties = animal_properties
-        self.distance_to_obstacle = 1
+        self.distance_to_obstacle = 0.4
         self.ahead_bounds = (np.radians(290), np.radians(70))
 
     def get_velocity_and_omega(self, scan: tuple):
         ranges = scan[0]
         angles = scan[1]
-        return self.animal_properties.max_linear_vel, self.__collision_avoidance(ranges, angles)        
+        return self.animal_properties.max_linear_vel, self.__collision_avoidance(ranges, angles)       
 
     def __collision_avoidance(self, ranges, angles):
         if len(ranges) == 0: # can happen when all obstacles are further away than laser can scan
             omega = 0
-        elif (ranges[np.logical_or(angles > np.radians(self.ahead_bounds[0]), angles < self.ahead_bounds[1])] > self.distance_to_obstacle).all(): # if distance to obstacle is big, just drive forward
+        elif (ranges[np.logical_or(angles > self.ahead_bounds[0], angles < self.ahead_bounds[1])] > self.distance_to_obstacle).all(): # if distance to obstacle is big, just drive forward
             omega = 0
         else:
             # normalize ranges 
-            norm_ranges = (ranges - np.min(ranges)) / (np.max(ranges) - np.min(ranges))
+            norm_ranges = (ranges - np.min(ranges)) / (np.max(ranges) - np.min(ranges) + 0.01)
 
             # compute the angular command velocity omega (turning rate)
             k = 0.001
-            force = (4/norm_ranges - 1) * k 
+            force = (4/(norm_ranges + 0.01) - 1) * k 
             force_angles = angles - np.pi # opposite force direction than sensor direction
             force_y = np.sin(force_angles) * force
-            force_y = np.sum(force_y) # or use np.mean()
-            c = 3.0
+            force_y = np.sum(force_y)
+            
+            c = 0.5
             omega_max = 2.84
             omega = np.clip(force_y * c, -omega_max, omega_max)
-
+        
         return omega

gruppe2/scripts/follow_plan_behaviour.py
@@ -19,7 +19,7 @@ class FollowPlanBehaviour():
         self.last_plan_other_pos = None
         self.first_plan_time = None
         self.beginning_plan_regenerated = False
-        self.pos_tolerance = 0.5
+        self.pos_tolerance = 0.5 # TODO make smaller
         self.path_driven = []
         self.goal_pub = rospy.Publisher("/cat_goal", PoseStamped, queue_size=10)
         self.cat_to_goal_pub = rospy.Publisher("/cat_to_goal", PoseStamped, queue_size=10)
@@ -118,19 +118,9 @@ class FollowPlanBehaviour():
                         break
 
                     goal_pos = self.__get_pos_from_pose_stamed(plan.poses[pose_index])
-                    print("NEXT INDEX: ", pose_index)
 
                 # drive towards goal pos
-                own_orientation_vec = np.array([np.cos(last_own_pos.orientation), np.sin(last_own_pos.orientation)])
-                own_orientation_vec /= np.linalg.norm(own_orientation_vec)
-
-                other_to_own_vector = np.array([-last_own_pos.pos.x + goal_pos.pos.x, -last_own_pos.pos.y + goal_pos.pos.y])
-                other_to_own_vector /= np.linalg.norm(other_to_own_vector)
-
-                angle = np.arctan2(other_to_own_vector[1] - own_orientation_vec[1], other_to_own_vector[0] - own_orientation_vec[0])
-                angle = angle + last_own_pos.orientation + np.pi
-
-                print(np.rad2deg(last_own_pos.orientation), np.rad2deg(angle))
+                angle_to_turn = get_angle_between_positions(last_own_pos, goal_pos)
 
                 # this is for RVIZ visualization only
                 try:
@@ -140,7 +130,7 @@ class FollowPlanBehaviour():
                     cat_pose.header.stamp = rospy.Time.now()
                     cat_pose.pose.position.x = last_own_pos.pos.x
                     cat_pose.pose.position.y = last_own_pos.pos.y
-                    [a, b, c, d] = quaternion_from_euler(0, 0, angle)
+                    [a, b, c, d] = quaternion_from_euler(0, 0, angle_to_turn + last_own_pos.orientation)
                     cat_pose.pose.orientation.x = a
                     cat_pose.pose.orientation.y = b
                     cat_pose.pose.orientation.z = c
@@ -150,7 +140,7 @@ class FollowPlanBehaviour():
                     print(ecx)
 
 
-                yield self.animal_properties.max_linear_vel, angle
+                yield self.animal_properties.max_linear_vel, angle_to_turn
 
 
     def __pos_inside_driven_path(self, pos):

gruppe2/scripts/helper_types.py
@@ -15,16 +15,12 @@ class PosAndOrientation:
         self.pos = Positition2D(x, y)
         self.orientation: float = orientation
 
-def get_angle_between_positions(own_pos: PosAndOrientation, other_pos: PosAndOrientation):
-    own_orientation_vec = np.array([np.cos(own_pos.orientation), np.sin(own_pos.orientation)])
-    own_orientation_vec /= np.linalg.norm(own_orientation_vec)
-
-    other_to_own_vector = np.array([other_pos.pos.x - own_pos.pos.x, other_pos.pos.y - own_pos.pos.y])
-    other_to_own_vector /= np.linalg.norm(other_to_own_vector)
-
-    angle_to_other_position = np.arccos(np.dot(own_orientation_vec, other_to_own_vector))
-
-    return angle_to_other_position
+def get_angle_between_positions(pos_one: PosAndOrientation, pow_two: PosAndOrientation):
+    x_diff = pow_two.pos.x - pos_one.pos.x
+    y_diff = pow_two.pos.y - pos_one.pos.y
+    angle_towards_second_pos = np.arctan2(y_diff, x_diff)
+    
+    return np.arctan2(np.sin(angle_towards_second_pos - pos_one.orientation), np.cos(angle_towards_second_pos - pos_one.orientation))
 
 def get_distance_between_positions(pos_one: Positition2D, pos_two: Positition2D):
     return np.sqrt((pos_two.x - pos_one.x) ** 2 + (pos_two.y - pos_one.y) ** 2)

gruppe2/scripts/minimax_behaviour.py
@@ -1,4 +1,5 @@
-from gruppe2.scripts.mouse_tracker import MouseTracker
+from typing import Counter
+from mouse_tracker import MouseTracker
 from helper_types import PosAndOrientation, AnimalProperties, Positition2D, get_angle_between_positions, get_distance_between_positions
 
 import numpy as np
@@ -13,7 +14,7 @@ class MinimaxBehaviour():
 
         # anymore and calculations take too long
         self.choices = 7
-
+        
         self.strategy_choices = np.linspace(-self.animal_properties.max_omega, self.animal_properties.max_omega, self.choices)
 
     def get_velocity_and_omega(self, own_pos: PosAndOrientation, mouse_tracker: MouseTracker):
@@ -24,7 +25,7 @@ class MinimaxBehaviour():
         self.current_enemy_properties = mouse_tracker.get_mouse_capabilities() # enemy capabilities could incorporate more info than just properties in the future, but for now just save it as is
         self.current_enemy_choices = np.linspace(-self.current_enemy_properties.max_omega, self.current_enemy_properties.max_omega, self.choices)
 
-        all_move_values = [self._minimax(choice, own_pos, other_pos, self.tree_depth, mean_enemy_vel, cheese_target, True) for choice in self.strategy_choices]
+        all_move_values = [self._minimax(choice, own_pos, other_pos, mean_enemy_vel, cheese_target, self.tree_depth, True) for choice in self.strategy_choices]
 
         return self.animal_properties.max_linear_vel, self.strategy_choices[np.argmin(all_move_values)]
 
@@ -37,7 +38,7 @@ class MinimaxBehaviour():
         if optimize_for_cat:            
             value = np.inf
             for choice in self.strategy_choices:
-                new_value = self._minimax(choice, new_own_pos, new_other_pos, cheese_target, depth-1, False, alpha, beta)
+                new_value = self._minimax(choice, new_own_pos, new_other_pos, mean_enemy_vel, cheese_target, depth-1, False, alpha, beta)
                 value = min(value, new_value)
                 beta = min(beta, value)
 
@@ -47,7 +48,7 @@ class MinimaxBehaviour():
         else:
             value = -np.inf
             for choice in self.current_enemy_choices:
-                new_value = self._minimax(choice, new_own_pos, new_other_pos, cheese_target, depth-1, True, alpha, beta)
+                new_value = self._minimax(choice, new_own_pos, new_other_pos, mean_enemy_vel, cheese_target, depth-1, True, alpha, beta)
                 value = max(value, new_value)
                 alpha = max(alpha, value)
 
@@ -56,13 +57,11 @@ class MinimaxBehaviour():
 
         return value
 
-
     def _cat_metric(self, own_pos: PosAndOrientation, other_pos: PosAndOrientation, cheese_target: Positition2D):
         angle = np.rad2deg(get_angle_between_positions(own_pos, other_pos))
-
         # check whether other animal is in front or behind us
         # use a little less than 180 deg to mean 'in front'
-        other_behind_own = angle < 290 and angle > 70
+        other_behind_own = angle > 70 or angle < -70
         
         if other_behind_own:
             distance_weight = 0.3
@@ -77,14 +76,14 @@ class MinimaxBehaviour():
         distance = (distance - 0) / (25 - 0)
 
         # normalize angle
-        angle = angle / 360
+        angle = np.abs(angle / 180)
 
         # distance between mouse and cheese, big distance is better
         mouse_cheese_distance = get_distance_between_positions(other_pos.pos, cheese_target)
 
         # emphasize distance and a good angle and weight it based on the fact if other animal (mouse) is behind or in front of ourselves (cat)
         #return np.abs(180 - angle)/-180
-        return distance_weight * distance + angle_weight * (1 - np.abs(0.5 - angle)) + mouse_cheese_distance
+        return distance_weight * distance + angle_weight * angle + mouse_cheese_distance
     
     def _mouse_metric(self, own_pos, other_pos, cheese_target: Positition2D):
         # it could be (i.e. it is pretty much given) that the mouse uses another system than we are, 

gruppe2/scripts/mouse_tracker.py
@@ -29,7 +29,7 @@ class MouseTracker():
         min_dist = np.inf
         min_dist_idx = 0
         for idx, cheese in enumerate(self.cheese_pos):
-            if get_distance_between_positions(self.mouse_pos, Positition2D(cheese[0], cheese[1])) < min_dist:
+            if get_distance_between_positions(self.mouse_pos.pos, Positition2D(cheese[0], cheese[1])) < min_dist:
                 min_dist_idx = idx
 
         # TODO future: maybe get other nearby cheese positions and decide based on viewing angle??